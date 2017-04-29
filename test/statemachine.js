a5879e4a2c5974e541e6606bd6037d242b3f5284
Massimiliano Fanciulli <fanciulli@gmail.com>
2017-04-29 09:08:21 +0200

    Updating test

--- test/statemachine.js
+++ test/statemachine.js
@@ -4,39 +4,7 @@ var fs = require('fs-extra');
 var standard = require('standard');
 var should = require('should');
 
-
-
-describe("Checking Queue", function () {
-    var statemachine;
-    var commandRouter;
-
-    before(function() {
-        commandRouter= new (require("../test/mockup/commandRouterMockup.js"))();
-        statemachine= new (require("../app/statemachine.js"))(commandRouter);
-    });
-
-
-    it("Checking that queue can be retrieved", function (done) {
-        this.timeout(300000);
-
-        var queue=statemachine.getQueue();
-        queue.should.be.instanceof(Array).and.have.lengthOf(0);
-
-        var queueItems=[{item:1,uri:""},{item:2,uri:""},{item:3,uri:""}];
-        var promise=statemachine.addQueueItems(queueItems);
-
-        promise.then(function()
-        {
-             queue = statemachine.getQueue();
-             queue.should.be.instanceof(Array).and.have.lengthOf(3);
-             done();
-        });
-    });
-});
-
-
-
-/*describe('Checking code styles', function () {
+describe('Checking code styles', function () {
     it('Checking that code styles are followed', function (done) {
         this.timeout(300000);
 
@@ -75,4 +43,35 @@ describe("Checking Queue", function () {
 
 
     });
+});
+
+/*describe("Checking Queue", function () {
+    var statemachine;
+    var commandRouter;
+
+    before(function() {
+        commandRouter= new (require("../test/mockup/commandRouterMockup.js"))();
+        statemachine= new (require("../app/statemachine.js"))(commandRouter);
+    });
+
+
+    it("Checking that queue can be retrieved", function (done) {
+        this.timeout(300000);
+
+        var queue=statemachine.getQueue();
+        queue.should.be.instanceof(Array).and.have.lengthOf(0);
+
+        var queueItems=[{item:1,uri:""},{item:2,uri:""},{item:3,uri:""}];
+        var promise=statemachine.addQueueItems(queueItems);
+
+        promise.then(function()
+        {
+             queue = statemachine.getQueue();
+             queue.should.be.instanceof(Array).and.have.lengthOf(3);
+             done();
+        });
+    });
 });*/
+
+
+
